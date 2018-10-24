bd118b27b68f2db1b96a46c542a7d5454aaff702
Brooke Perry and Christopher Cahill <christopher@>
2018-10-24 13:46:40 -0700

    switch to make string inputs

--- js/scripts.js
+++ js/scripts.js
@@ -6,18 +6,25 @@ function makeInputArray(inputString, separator) {
 
 function pigLatinTranslator(inputArray, vowels) {
   var newArray =[];
+    // if (exceptionsArray.includes((inputArray[1]).toString())) {
+    //   newArray.push(inputArray.join(''));
+    //
+    // }
+    // else if (vowels.includes((inputArray[1]).toString())) {
+    //   newArray.push(inputArray.join(''));
+    //   newArray.push("way");
+    // }
   for (var i = 0; i <= inputArray.length-1 ; i++) {
     if (vowels.includes(inputArray[i]))
     {
-      newArray.push(inputArray[i]+"way") }
+      newArray.push(inputArray.concat(["w", "a", "y"]))}
     }
-
   return newArray;
 }
 
 var vowelArray = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
 var consonantArray = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Z"];
-var exceptionsArray = [];
+var exceptionsArray = ["y", "Y"];
 
 
 $(document).ready(function(){
@@ -28,6 +35,7 @@ $(document).ready(function(){
   var userArray = makeInputArray(userString, "");
   var result = pigLatinTranslator(userArray, vowelArray);
 
+  console.log(userArray);
   console.log(result);
 
   });
